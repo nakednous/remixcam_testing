d8fc09745439c8fb91888ebd5c2c40b3655a74c2
nakednous <nakednous@gmail.com>
2013-12-10 17:31:50 -0500

    viewPoint.setKFI is more capable now

--- src/basic/CloningKFIs.java
+++ src/basic/CloningKFIs.java
@@ -9,20 +9,23 @@ import remixlab.dandelion.geom.*;
 
 @SuppressWarnings("serial")
 public class CloningKFIs extends PApplet {
-	Scene scene, auxScene;
+	Scene mainScene, auxScene;
 	PGraphics canvas, auxCanvas;
+	
+	boolean mainMouse, auxMouse;
 
 	public void setup() {
 		size(640, 720, P3D);
 
 		canvas = createGraphics(640, 360, P3D);
-		scene = new Scene(this, canvas);
+		mainScene = new Scene(this, canvas);
+		mainScene.setRadius(mainScene.radius()*1.6f);
 		
-		scene.defaultKeyboardAgent().profile().setShortcut('v', KeyboardAction.CAMERA_KIND);
+		mainScene.defaultKeyboardAgent().profile().setShortcut('v', KeyboardAction.CAMERA_KIND);
 		// enable computation of the frustum planes equations (disabled by default)
-		scene.enableFrustumEquationsUpdate();
-		scene.setGridIsDrawn(false);
-		scene.addDrawHandler(this, "mainDrawing");
+		mainScene.enableFrustumEquationsUpdate();
+		mainScene.setGridIsDrawn(false);
+		mainScene.addDrawHandler(this, "mainDrawing");
 		
 		auxCanvas = createGraphics(640, 360, P3D);
 		// Note that we pass the upper left corner coordinates where the scene
@@ -36,6 +39,10 @@ public class CloningKFIs extends PApplet {
 		auxScene.showAll();
 		auxScene.addDrawHandler(this, "auxiliarDrawing");
 
+		unregisterMethod("mouseEvent", mainScene.defaultMouseAgent());
+		unregisterMethod("mouseEvent", auxScene.defaultMouseAgent());
+		unregisterMethod("keyEvent", mainScene.defaultKeyboardAgent());
+		unregisterMethod("keyEvent", auxScene.defaultKeyboardAgent());
 		handleMouse();
 	}
 
@@ -44,7 +51,7 @@ public class CloningKFIs extends PApplet {
 		p.background(0);
 		p.noStroke();
 		// the main viewer camera is used to cull the sphere object against its frustum
-		switch (scene.camera().sphereIsVisible(new Vec(0, 0, 0), 40)) {
+		switch (mainScene.camera().sphereIsVisible(new Vec(0, 0, 0), 40)) {
 		case VISIBLE:
 			p.fill(0, 255, 0);
 			p.sphere(40);
@@ -63,15 +70,15 @@ public class CloningKFIs extends PApplet {
 		s.pg3d().pushStyle();
 		s.pg3d().stroke(255,255,0);
 		s.pg3d().fill(255,255,0,160);
-		s.drawCamera(scene.camera());
+		s.drawCamera(mainScene.camera());
 		s.pg3d().popStyle();
 	}
 
 	public void draw() {
 		handleMouse();
 		canvas.beginDraw();
-		scene.beginDraw();
-		scene.endDraw();
+		mainScene.beginDraw();
+		mainScene.endDraw();
 		canvas.endDraw();
 		image(canvas, 0, 0);
 
@@ -83,41 +90,68 @@ public class CloningKFIs extends PApplet {
 		image(auxCanvas, auxScene.upperLeftCorner.x, auxScene.upperLeftCorner.y);
 	}
 	
+	//Agent should be available all the time:
 	public void handleMouse() {
 		if (mouseY < 360) {
-			scene.enableDefaultMouseAgent();
-			scene.enableDefaultKeyboardAgent();
-			auxScene.disableDefaultMouseAgent();
-			auxScene.disableDefaultKeyboardAgent();
+			if(!mainMouse) {
+			  registerMethod("mouseEvent", mainScene.defaultMouseAgent());
+			  registerMethod("keyEvent", mainScene.defaultKeyboardAgent());			  
+			  mainMouse = true;
+			  auxMouse = false;
+			}			
+			unregisterMethod("mouseEvent", auxScene.defaultMouseAgent());
+			unregisterMethod("keyEvent", auxScene.defaultKeyboardAgent());
+			
 		} else {
-			scene.disableDefaultMouseAgent();
-			scene.disableDefaultKeyboardAgent();
-			auxScene.enableDefaultMouseAgent();
-			auxScene.enableDefaultKeyboardAgent();
+			if(!auxMouse) {
+				registerMethod("mouseEvent", auxScene.defaultMouseAgent());
+				registerMethod("keyEvent", auxScene.defaultKeyboardAgent());
+				mainMouse = false;
+				auxMouse = true;
+			}
+			unregisterMethod("mouseEvent", mainScene.defaultMouseAgent());
+			unregisterMethod("keyEvent", mainScene.defaultKeyboardAgent());
 		}
 	}
 	
 	public void keyPressed() {
-		/*
 		if (key == 'x') {
-		    KeyFrameInterpolator kfiOriginal = scene.camera().keyFrameInterpolator(1);		    
-		    KeyFrameInterpolator kfiNew = kfiOriginal.get();
-		    scene.camera().setKeyFrameInterpolator(2, kfiNew);
+		    KeyFrameInterpolator kfiOriginal = mainScene.camera().keyFrameInterpolator(1);	
+		    if(kfiOriginal != null) {
+		    	KeyFrameInterpolator kfiNew = kfiOriginal.get();
+		    	mainScene.camera().setKeyFrameInterpolator(2, kfiNew);
+		    }
 		}
-		// */
 		if (key == 'y') {
-		    //KeyFrameInterpolator kfiOriginal = scene.camera().keyFrameInterpolator(1);		    
-		    //KeyFrameInterpolator kfiNew = kfiOriginal.get();
-			KeyFrameInterpolator kfiNew = scene.camera().keyFrameInterpolator(1).get();
-		    
+		    /*
+			KeyFrameInterpolator kfiNew = mainScene.camera().keyFrameInterpolator(1).get();		    
 		    kfiNew.scene = auxScene;
-		    kfiNew.setFrame(auxScene.camera().frame());
 		    
+		    kfiNew.setFrame(auxScene.camera().frame());		    
 		    for (int i = 0; i < kfiNew.numberOfKeyFrames(); ++i)
 		    	  if(kfiNew.keyFrame(i) instanceof InteractiveFrame)
-		    	    ((InteractiveFrame)kfiNew.keyFrame(i)).scene = auxScene;
-		    
+		    	    ((InteractiveFrame)kfiNew.keyFrame(i)).scene = auxScene;		    
 		    auxScene.camera().setKeyFrameInterpolator(1, kfiNew);
+		    //*/
+			
+			KeyFrameInterpolator kfiOrig = mainScene.camera().keyFrameInterpolator(1);
+			if(kfiOrig != null)
+				auxScene.camera().setKeyFrameInterpolator(1, kfiOrig.get());
+			else
+				auxScene.camera().deletePath(1);
+		}
+		if(key=='z') {
+			KeyFrameInterpolator kfi;
+			kfi = mainScene.camera().keyFrameInterpolator(1);
+			if( kfi != null ) 
+				println("main scene path 1 no.: " + kfi.numberOfKeyFrames());
+			else
+				println("main scene path 1 no.: " + 0);
+			kfi = auxScene.camera().keyFrameInterpolator(1);
+			if( kfi != null ) 
+				println("aux scene path 1 no.: " + kfi.numberOfKeyFrames());
+			else
+				println("aux scene path 1 no.: " + 0);
 		}
 	}
 	
